3dc61c8ca373150b54b5c3eff8f69351dd4b326e
tylerphelps <tylerphelps@10.242.1.4>
2020-02-14 21:26:44 -0500

    ready to grade

---
+++ Phelps Program#1/src/program1.java
@@ -0,0 +1,38 @@
+//  Name:   Project 1
+//  Author: Tyler Phelps
+//  Date:   Feb 13, 2020
+//  Version: 1.0
+//  Git User: typhelps2
+
+public class program1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        // Declaring Variables
+		int age = 19; 
+		byte favoriteNumber = 2; 
+		String name = "Tyler Phelps";
+		
+		//Output content to the screen.
+		System.out.print("Hey there.\n");
+		// System.out.println("");
+		System.out.print("My name is " + name);
+		System.out.println(" I am " + age + " years old. ");
+		System.out.println("My favorite number is " + favoriteNumber);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
